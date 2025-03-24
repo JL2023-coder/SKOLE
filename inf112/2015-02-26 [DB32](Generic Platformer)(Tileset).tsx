450f69f78019bb7e2a5b8cd8fe2e50440e223f5d
JL2023-coder <jonas.lilletvedt@icloud.com>
2025-03-24 20:11:38 +0100

    Fjernet uønskede mapper fra Git-versjonskontrollen

--- inf112/2015-02-26 [DB32](Generic Platformer)(Tileset).tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.11.2" name="2015-02-26 [DB32](Generic Platformer)(Tileset)" tilewidth="16" tileheight="16" tilecount="144" columns="16">
- <image source="../Desktop/2015-02-26 [DB32](Generic Platformer)(Tileset).png" width="256" height="144"/>
-</tileset>
