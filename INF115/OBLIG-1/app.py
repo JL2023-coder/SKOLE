f3c615b3f44bfb392d08d99de945f5aef8d44b01
JL2023-coder <jonas.lilletvedt@icloud.com>
2025-03-28 16:13:13 +0100

    Ferdig

--- INF115/OBLIG-1/app.py
+++ INF115/OBLIG-1/app.py
@@ -58,38 +58,49 @@ def get_task_c():
     con = sqlite3.connect('bysykkel.db')
 
     # Use parameterized query to prevent SQL injection
-    query = "SELECT bike_name, reperation_status FROM bike"
+    query = "SELECT subscription_type FROM subscription"
 
     # Execute query with the table name
     df = pd.read_sql_query(query, con)
 
+    # Count different subscription types and create a new DataFrame
+    count_df = df['subscription_type'].value_counts().reset_index()
+    count_df.columns = ['Type', 'count']
+
+    # Sort based on the custom order
+    count_df = count_df.sort_values(by='Type')
+
     # Close the connection
     con.close()
 
-    return df
+    return count_df
 
+# Chooses correct table based in user input
 def get_table_from_task(task):
     if task == "a": 
         return get_task_a()
     elif task == "b":
         return get_task_b()
-    elif task == "(c)":
+    elif task == "c":
         return get_task_c()
     else:
         return pd.DataFrame()
         
 
-
+# Allows user input
 ui.input_selectize(  
     "selectize",  
     "Velg oppgave a, b eller c:",  
-    {"a": "User names (a)", "b": "Bike names with status (b)", "(c)": "Subscription purchases (c)"}  
+    {"a": "User names (a)", "b": "Bike names with status (b)", "c": "Subscription purchases (c)"}  
 ) 
 
+# Renders table
 ui.h1("BYSYKKEL DATABSE:)")
 @render.data_frame
 def render_table1():
+    # User input through selectize
     inp = input.selectize()
+    # Shows coorect table based on user input
     table_df = get_table_from_task(inp)
     return table_df
 
